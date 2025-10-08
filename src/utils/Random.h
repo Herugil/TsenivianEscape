e18a42f7dc894c2375eb2b7aa040851f8444edc2
Herugil <jerome.viteau@gmail.com>
2025-10-08 19:49:30 +0200

    Add npc combat turn test

--- src/utils/Random.h
+++ src/utils/Random.h
@@ -4,32 +4,48 @@
 #include <random>
 
 namespace Random {
-inline std::mt19937 generate() {
-  std::random_device rd{};
 
-  // Create seed_seq with clock and 7 random numbers from std::random_device
-  std::seed_seq ss{
-      static_cast<std::seed_seq::result_type>(
-          std::chrono::steady_clock::now().time_since_epoch().count()),
-      rd(),
-      rd(),
-      rd(),
-      rd(),
-      rd(),
-      rd(),
-      rd()};
+// Separate generator class for testability
+class Generator {
+private:
+  std::mt19937 m_mt;
 
-  return std::mt19937{ss};
-}
-inline std::mt19937 mt{generate()};
-inline int get(int min, int max) {
-  return std::uniform_int_distribution{min, max}(mt);
-}
+public:
+  Generator() : m_mt(generateSeed()) {}
+  explicit Generator(unsigned int seed) : m_mt(seed) {}
+  int get(int min, int max) {
+    return std::uniform_int_distribution{min, max}(m_mt);
+  }
+  template <typename T> T get(T min, T max) {
+    return std::uniform_int_distribution<T>{min, max}(m_mt);
+  }
+  int rollD100() { return get(1, 100); }
+  void setSeed(unsigned int seed) { m_mt.seed(seed); }
 
+private:
+  static std::mt19937 generateSeed() {
+    std::random_device rd{};
+    std::seed_seq ss{
+        static_cast<std::seed_seq::result_type>(
+            std::chrono::steady_clock::now().time_since_epoch().count()),
+        rd(),
+        rd(),
+        rd(),
+        rd(),
+        rd(),
+        rd(),
+        rd()};
+    return std::mt19937{ss};
+  }
+};
+
+inline Generator g_generator;
+inline int get(int min, int max) { return g_generator.get(min, max); }
 template <typename T> inline T get(T min, T max) {
-  return std::uniform_int_distribution<T>{min, max}(mt);
+  return g_generator.get<T>(min, max);
 }
 
-inline int rollD100() { return get(1, 100); }
+inline int rollD100() { return g_generator.rollD100(); }
+inline void setSeed(unsigned int seed) { g_generator.setSeed(seed); }
 
 } // namespace Random
--- tests/testCombat.cpp
+++ tests/testCombat.cpp
@@ -7,6 +7,7 @@ class CombatTest : public testing::Test {
 protected:
   std::unique_ptr<GameSession> gameSession;
   void SetUp() override {
+    Random::setSeed(42);
     gameSession = std::make_unique<GameSession>(createTestGameSession());
     gameSession->addNpc(createTestNPC());
   }
@@ -73,4 +74,40 @@ TEST_F(CombatTest, BaseEnemyActions) {
   ASSERT_NE(npc, nullptr);
   NpcCombatAI::npcActCombat(*gameSession, npc);
   ASSERT_EQ(npc->getCurrentAction()->getName(), "Precise Strikes");
+}
+
+TEST_F(CombatTest, EnemyTurns) {
+  // This test checks that the npc which is support
+  // buffs itself first, then gets close to the player
+  // and eventually attacks, dealing 2 damage
+  gameSession->initializeTurnOrder();
+  gameSession->incrementTurnIndex();
+  auto activeCreature{gameSession->getActiveCreature().lock()};
+  ASSERT_NE(activeCreature, nullptr);
+  auto npc{std::dynamic_pointer_cast<NonPlayableCharacter>(activeCreature)};
+  ASSERT_NE(npc, nullptr);
+  while (npc->getCurrentBehavior() != NonPlayableCharacter::skipTurn) {
+    NpcCombatAI::npcActCombat(*gameSession, npc);
+  }
+  ASSERT_EQ(npc->getStatModifier(Stat::MeleeHitChance), 10);
+  npc->resetTurn();
+  gameSession->incrementTurnIndex();
+  ASSERT_EQ(gameSession->getActiveCreature().lock(),
+            gameSession->getPlayerPtr());
+  gameSession->incrementTurnIndex();
+  NpcCombatAI::npcActCombat(*gameSession, npc);
+  while (npc->getCurrentBehavior() != NonPlayableCharacter::skipTurn) {
+    NpcCombatAI::npcActCombat(*gameSession, npc);
+  }
+  ASSERT_EQ(gameSession->getPlayer().getHealthPoints(), 10);
+  npc->resetTurn();
+  gameSession->incrementTurnIndex();
+  gameSession->incrementTurnIndex();
+
+  // the first action allows to unset skip turn if npc can act again
+  NpcCombatAI::npcActCombat(*gameSession, npc);
+  while (npc->getCurrentBehavior() != NonPlayableCharacter::skipTurn) {
+    NpcCombatAI::npcActCombat(*gameSession, npc);
+  }
+  ASSERT_EQ(gameSession->getPlayer().getHealthPoints(), 8);
 }
