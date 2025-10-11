f2e0ce392c2d73041b7d12bdbfaf557541daa817
Herugil <jerome.viteau@gmail.com>
2025-10-11 09:41:14 +0200

    Fix ignoring warnings

--- tests/fixtures/fixtures.h
+++ tests/fixtures/fixtures.h
@@ -12,6 +12,7 @@
 #pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Wextra"
 #pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
 #elif defined(_MSC_VER)
 #pragma warning(push, 0) // Disable all warnings (level 0)
 #endif
